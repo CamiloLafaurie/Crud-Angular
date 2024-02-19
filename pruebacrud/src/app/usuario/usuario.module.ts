ba716ab0e1b48b742ef3fa2f50a88e7c9034eaf1
CamiloLafaurie <camilolafaurie7@gmail.com>
2024-02-19 18:54:23 -0500

    elinimar curd antiguo

--- pruebacrud/src/app/usuario/usuario.module.ts
+++
@@ -1,9 +0,0 @@
-export interface Usuarios {
-  identificacion: string;
-  tipoIdentificacion: string;
-  nombre: string;
-  apellido: string;
-  correo: string;
-  edad:string;
-  fechaNacimiento?: Date;
-}
